47b6b332e0eead81bc773f601e60cce1539641d6
vinu tv <vinutv91@gmail.com>
2023-06-16 12:32:12 +0530

    Fix in vite-config Access-Control-Allow-Credentials

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
-
-module.exports = {
   server: {
     middleware: {
       "/api": {
@@ -21,4 +18,4 @@ module.exports = {
       },
     },
   },
-};
+});
