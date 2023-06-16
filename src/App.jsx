64596444d406dea547163616ad7a9513688fc1ba
vinu tv <vinutv91@gmail.com>
2023-06-16 12:56:01 +0530

    Add vercel.json

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { Experience } from "./components/Experience";
 import { Stage, Grid, Environment, Backdrop } from '@react-three/drei'
 
 
-
 function App() {
 // const { color } = useControls({ color: '#40006A' })
   return (
@@ -14,14 +13,14 @@ function App() {
       <directionalLight position={[-1, -2, -5]} intensity={0.2} color="#0c8cbf" />
       <spotLight position={[5, 0, 5]} intensity={2.5} penumbra={1} angle={0.35} castShadow color="#2b0080" />
 
-      <Stage intensity={0.2} files={'/environmentMaps/venice_sunset_1k.hdr'} shadows={{ type: 'contact', colorBlend: 5, opacity: 5 }} adjustCamera={false} background="#2b0080">
+      <Stage intensity={0.2} environment="sunset" shadows={{ type: 'contact', colorBlend: 5, opacity: 5 }} adjustCamera={false} background="#2b0080">
         <Experience/>
       </Stage>
       {/* <Backdrop receiveShadow scale={[70, 25, 15]} floor={3} position={[0, -5, -10]}>
         <meshPhysicalMaterial roughness={1} color="#40006A" />
       </Backdrop> */}
       <Grid renderOrder={-1} position={[0, -4.8, 0]} infiniteGrid cellSize={0.6} cellThickness={0.6} sectionSize={3.3} sectionThickness={1.5} sectionColor={[0.5, 0.5, 1]} fadeDistance={50} />
-      <Environment background files={'/environmentMaps/venice_sunset_1k.hdr'} blur={0.8} />
+      <Environment background preset="sunset" blur={0.8} />
     </Canvas>
   );
 }
