4ce20e61ba98f3e26749a1fcabe19837cc71b188
vinu tv <vinutv91@gmail.com>
2023-06-16 14:39:02 +0530

    Remove vercel.json

--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/market-assets": {
-        target:
-          "https://market-assets.fra1.cdn.digitaloceanspaces.com/market-assets/hdris/",
-        changeOrigin: true,
-        headers: {
-          "Access-Control-Allow-Credentials": "true",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods":
-            "GET, OPTIONS, PATCH, DELETE, POST, PUT",
-          "Access-Control-Allow-Headers":
-            "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-        },
-      },
-    },
-  },
 });
